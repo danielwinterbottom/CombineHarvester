5329e8867bc8436aa5bd225bfda6b93c162a5deb
Daniel <d.winterbottom15@imperial.ac.uk>
2017-02-07 12:11:33 +0000

    hopefully fixed isse with limits - checked for mt and ggH only currently though

--- HIG16037/bin/MorphingMSSM2016.cpp
+++ HIG16037/bin/MorphingMSSM2016.cpp
@@ -19,7 +19,7 @@
 #include "CombineHarvester/CombineTools/interface/Algorithm.h"
 #include "CombineHarvester/CombineTools/interface/AutoRebin.h"
 #include "CombineHarvester/CombinePdfs/interface/MorphFunctions.h"
-#include "CombineHarvester/IsoCats/interface/HttSystematics_MSSMRun2.h"
+#include "CombineHarvester/HIG16037/interface/HttSystematics_MSSMRun2.h"
 #include "RooWorkspace.h"
 #include "RooRealVar.h"
 #include "TH2.h"
@@ -105,13 +105,13 @@ int main(int argc, char** argv) {
   typedef vector<string> VString;
   typedef vector<pair<int, string>> Categories;
   std::map<string, string> input_dir;
-  input_dir["em"]  = string(getenv("CMSSW_BASE")) + "/src/CombineHarvester/IsoCats/shapes/"+input_folder_em+"/";
-  input_dir["mt"]  = string(getenv("CMSSW_BASE")) + "/src/CombineHarvester/IsoCats/shapes/"+input_folder_mt+"/";
-  input_dir["et"]  = string(getenv("CMSSW_BASE")) + "/src/CombineHarvester/IsoCats/shapes/"+input_folder_et+"/";
-  input_dir["tt"]  = string(getenv("CMSSW_BASE")) + "/src/CombineHarvester/IsoCats/shapes/"+input_folder_tt+"/";
-  input_dir["zmm"]  = string(getenv("CMSSW_BASE")) + "/src/CombineHarvester/IsoCats/shapes/"+input_folder_zmm+"/";
+  input_dir["em"]  = string(getenv("CMSSW_BASE")) + "/src/CombineHarvester/HIG16037/shapes/"+input_folder_em+"/";
+  input_dir["mt"]  = string(getenv("CMSSW_BASE")) + "/src/CombineHarvester/HIG16037/shapes/"+input_folder_mt+"/";
+  input_dir["et"]  = string(getenv("CMSSW_BASE")) + "/src/CombineHarvester/HIG16037/shapes/"+input_folder_et+"/";
+  input_dir["tt"]  = string(getenv("CMSSW_BASE")) + "/src/CombineHarvester/HIG16037/shapes/"+input_folder_tt+"/";
+  input_dir["zmm"]  = string(getenv("CMSSW_BASE")) + "/src/CombineHarvester/HIG16037/shapes/"+input_folder_zmm+"/";
 
-  VString chns = {"mt","et"};
+  VString chns = {"mt"};
   if (zmm_fit) chns.push_back("zmm");
 
   RooRealVar mA(mass.c_str(), mass.c_str(), 90., 3200.);
@@ -230,20 +230,14 @@ int main(int argc, char** argv) {
   // the vector below specifies a bin name and corresponding bin_id.
   //
   map<string,Categories> cats;
-  cats["et_13TeV"] = {
 
-    {8, "et_nobtag_loose_5"},
-    {9, "et_nobtag_tight_5"},
-    {10,"et_btag_loose_5"},
-    {11,"et_btag_tight_5"},
-    {12,"et_nobtag_loose_6"},
-    {13,"et_nobtag_tight_6"},
-    {14,"et_btag_loose_6"},
-    {15,"et_btag_tight_6"},
+  cats["et_13TeV"] = {
     {16,"et_nobtagtightmt50"},
     {17,"et_btagtightmt50"},
-    {18,"et_nobtagmediummt50"},
-    {19,"et_btagmediummt50"}
+    {18, "et_nobtag_loose_5"},
+    {19, "et_nobtag_tight_5"},
+    {20,"et_btag_loose_5"},
+    {21,"et_btag_tight_5"}
     
     };
 
@@ -258,18 +252,12 @@ int main(int argc, char** argv) {
     };
 
   cats["mt_13TeV"] = {
-    {8, "mt_nobtag_loose_5"},
-    {9, "mt_nobtag_tight_5"},
-    {10,"mt_btag_loose_5"},
-    {11,"mt_btag_tight_5"},
-    {12,"mt_nobtag_loose_6"},
-    {13,"mt_nobtag_tight_6"},
-    {14,"mt_btag_loose_6"},
-    {15,"mt_btag_tight_6"},
     {16,"mt_nobtagtightmt50"},
     {17,"mt_btagtightmt50"},
-    {18,"mt_nobtagmediummt50"},
-    {19,"mt_btagmediummt50"}
+    {18, "mt_nobtag_loose_5"},
+    {19, "mt_nobtag_tight_5"},
+    {20,"mt_btag_loose_5"},
+    {21,"mt_btag_tight_5"}
     };
 
   cats["zmm_13TeV"] = {
@@ -698,26 +686,19 @@ int main(int argc, char** argv) {
     // per-channel
     writer.WriteCards(chn, cb.cp().channel({chn, "zmm"}));
     // And per-channel-category
-    writer.WriteCards("htt_"+chn+"_8_13TeV", cb.cp().channel({chn,"zmm"}).bin_id({8}));
-    writer.WriteCards("htt_"+chn+"_9_13TeV", cb.cp().channel({chn,"zmm"}).bin_id({9}));
-    writer.WriteCards("htt_"+chn+"_10_13TeV", cb.cp().channel({chn,"zmm"}).bin_id({10}));
-    writer.WriteCards("htt_"+chn+"_11_13TeV", cb.cp().channel({chn,"zmm"}).bin_id({11}));
-    writer.WriteCards("htt_"+chn+"_12_13TeV", cb.cp().channel({chn,"zmm"}).bin_id({12}));
-    writer.WriteCards("htt_"+chn+"_13_13TeV", cb.cp().channel({chn,"zmm"}).bin_id({13}));
-    writer.WriteCards("htt_"+chn+"_14_13TeV", cb.cp().channel({chn,"zmm"}).bin_id({14}));
-    writer.WriteCards("htt_"+chn+"_15_13TeV", cb.cp().channel({chn,"zmm"}).bin_id({15}));
     writer.WriteCards("htt_"+chn+"_16_13TeV", cb.cp().channel({chn,"zmm"}).bin_id({16}));
     writer.WriteCards("htt_"+chn+"_17_13TeV", cb.cp().channel({chn,"zmm"}).bin_id({17}));
     writer.WriteCards("htt_"+chn+"_18_13TeV", cb.cp().channel({chn,"zmm"}).bin_id({18}));
     writer.WriteCards("htt_"+chn+"_19_13TeV", cb.cp().channel({chn,"zmm"}).bin_id({19}));
-    writer.WriteCards("htt_"+chn+"_isocats5_nobtag_ltcomb_13TeV", cb.cp().channel({chn,"zmm"}).bin_id({8,9}));
-    writer.WriteCards("htt_"+chn+"_isocats5_btag_ltcomb_13TeV", cb.cp().channel({chn,"zmm"}).bin_id({10,11}));
-    writer.WriteCards("htt_"+chn+"_isocats5_ltcomb_13TeV", cb.cp().channel({chn,"zmm"}).bin_id({8,9,10,11}));
-    writer.WriteCards("htt_"+chn+"_isocats6_nobtag_ltcomb_13TeV", cb.cp().channel({chn,"zmm"}).bin_id({12,13}));
-    writer.WriteCards("htt_"+chn+"_isocats6_btag_ltcomb_13TeV", cb.cp().channel({chn,"zmm"}).bin_id({14,15}));
-    writer.WriteCards("htt_"+chn+"_isocats6_ltcomb_13TeV", cb.cp().channel({chn,"zmm"}).bin_id({12,13,14,15}));
-    writer.WriteCards("htt_"+chn+"_tightmt50_comb_13TeV", cb.cp().channel({chn,"zmm"}).bin_id({16,17}));
-    writer.WriteCards("htt_"+chn+"_mediummt50_comb_13TeV", cb.cp().channel({chn,"zmm"}).bin_id({18,19}));
+    writer.WriteCards("htt_"+chn+"_20_13TeV", cb.cp().channel({chn,"zmm"}).bin_id({20}));
+    writer.WriteCards("htt_"+chn+"_21_13TeV", cb.cp().channel({chn,"zmm"}).bin_id({21}));
+    writer.WriteCards(chn+"_comb_16_17", cb.cp().channel({chn,"zmm"}).bin_id({16,17}));
+    writer.WriteCards(chn+"_comb_19_21", cb.cp().channel({chn,"zmm"}).bin_id({19,21}));
+    writer.WriteCards(chn+"_comb_18_16_20_21", cb.cp().channel({chn,"zmm"}).bin_id({18,16,20,17}));
+    writer.WriteCards(chn+"_comb_18_19_20_21", cb.cp().channel({chn,"zmm"}).bin_id({18,19,20,21}));
+    writer.WriteCards(chn+"_comb_18_19", cb.cp().channel({chn,"zmm"}).bin_id({18,19}));
+    writer.WriteCards(chn+"_comb_20_21", cb.cp().channel({chn,"zmm"}).bin_id({20,21}));
+    
     
   }
   // For btag/nobtag areas want to include control regions. This will
--- HIG16037/run.sh
+++ HIG16037/run.sh
@@ -3,11 +3,11 @@ MorphingMSSM2016 --output_folder=$1 -m MH --postfix="-mttot" --control_region=0 
 combineTool.py -M T2W -o "ws.root" -P HiggsAnalysis.CombinedLimit.PhysicsModel:multiSignalModel --PO '"map=^.*/ggH$:r_ggH[0,0,200]"' --PO '"map=^.*/bbH$:r_bbH[0,0,200]"' -i output/$1/*
 
 if [ $2 == 1 ];then
-    combineTool.py -m "90,100,110,120,130,140,160,180,200,250,350,400,450,500,700,800,900,1000,1200,1400,1600,1800,2000,2300,2600,2900,3200" -M Asymptotic --rAbsAcc 0 --rRelAcc 0.0005 --boundlist input/mssm_boundaries.json  --setPhysicsModelParameters r_ggH=0,r_bbH=0 --redefineSignalPOIs r_ggH -d output/$1/*/ws.root --there -n ".ggH" -t -1 --parallel=1
+    combineTool.py -m "90,100,110,120,130,140,160,180,200,250,350,400,450,500,700,800,900,1000,1200,1400,1600,1800,2000,2300,2600,2900,3200" -M Asymptotic --rAbsAcc 0 --rRelAcc 0.0005 --boundlist input/mssm_boundaries.json  --setPhysicsModelParameters r_ggH=0,r_bbH=0 --redefineSignalPOIs r_ggH -d output/$1/*/ws.root --there -n ".ggH" -t -1 --parallel=6
     #--job-mode 'lxbatch' --task-name $1_ggH --sub-opts '-q 1nh' --merge=2
 fi    
 if [ $3 == 1 ];then
-    combineTool.py -m "90,100,110,120,130,140,160,180,200,250,350,400,450,500,700,800,900,1000,1200,1400,1600,1800,2000,2300,2600,2900,3200" -M Asymptotic --rAbsAcc 0 --rRelAcc 0.0005 --boundlist input/mssm_boundaries.json  --setPhysicsModelParameters r_ggH=0,r_bbH=0 --redefineSignalPOIs r_bbH -d output/$1/*/ws.root --there -n ".bbH" -t -1 --parallel=1
+    combineTool.py -m "90,100,110,120,130,140,160,180,200,250,350,400,450,500,700,800,900,1000,1200,1400,1600,1800,2000,2300,2600,2900,3200" -M Asymptotic --rAbsAcc 0 --rRelAcc 0.0005 --boundlist input/mssm_boundaries.json  --setPhysicsModelParameters r_ggH=0,r_bbH=0 --redefineSignalPOIs r_bbH -d output/$1/*/ws.root --there -n ".bbH" -t -1 --parallel=6
     #--job-mode 'lxbatch' --task-name $1_bbH --sub-opts '-q 1nh' --merge=2
 fi
 
